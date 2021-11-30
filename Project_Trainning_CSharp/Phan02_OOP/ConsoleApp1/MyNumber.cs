581a55868f6c896c6afdc2a8f23788a8dfc8e34b
Nguyen Minh Phuc <nmphuc@outlook.com>
2021-11-30 21:49:35 +0700

    Commit all Changed oop

---
+++ Project_Trainning_CSharp/Phan02_OOP/ConsoleApp1/MyNumber.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectOOP
+{
+    public class MyNumber
+    {
+        public static int  Add(int a, int b)
+        {
+            return a + b;
+        }
+        public static int Subtrac(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
---
+++ Project_Trainning_CSharp/Phan02_OOP/ConsoleApp1/NhanVien.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectOOP
+{
+   public class NhanVien:Person
+    {
+      private  double heSoLuong;
+        public NhanVien() : base()
+        {
+
+        }
+        public NhanVien(string maNhanVien, string tenNhanVien,int tuoiNhanVien, string diaChi,double heSoLuong) : base(maNhanVien,tenNhanVien,tuoiNhanVien,diaChi)
+        {
+            this.heSoLuong = heSoLuong;
+        }
+
+        public override void NhapThongTinPerson()
+        {
+            base.NhapThongTinPerson();
+            Console.WriteLine("Nhap HeSoLuong: ");
+             heSoLuong=Convert.ToDouble( Console.ReadLine());
+        }
+        public override string ToString()
+        {
+            return base.ToString()+ string.Format("He so Luong {0}",heSoLuong);
+        }
+    }
+}
